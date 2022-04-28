c9f4c5aa486adb0ec8f5e16941ae8e0a285ed004
xFikrie <twitwit7@gmail.com>
2022-04-28 10:30:23 +0700

    favicon laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
 
     <title>{{ $title ? $title : config('app.name', 'Laravel') }}</title>
 
